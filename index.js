98bf8c361b29b4a2589ef9ebbce9b111c9466ede
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-03-05 15:08:04 -0500

    commit tree 0de828149de7577f3a80ec35e01c5b5bf801ee2b

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Write your solution here!
+function cats() {
+    return cats=["Milo", "Otis", "Garfield"]
+};
+
+console.log(cats());
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph")
+};
+
+console.log(destructivelyAppendCat());
+console.log(cats);
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob")
+}; 
+
+console.log(destructivelyPrependCat());
+console.log(cats);
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+};
+
+console.log(destructivelyRemoveLastCat());
+console.log(cats);
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+};
+
+console.log(destructivelyRemoveFirstCat());
+console.log(cats);
+
+function appendCat(name) {
+    return [...cats, "Broom"]
+};
+
+console.log(appendCat());
+
+function prependCat(name) {
+    return ["Arnold", ...cats]
+};
+
+console.log(prependCat());
+
+function removeLastCat() {
+    return cats.slice(0,-1)
+};
+
+console.log(removeLastCat());
+
+function removeFirstCat() {
+    return cats.slice(1,)
+};
+
+console.log(removeFirstCat());
